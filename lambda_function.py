a357b18e023403be0da3c2d236048bbc208a2751
박성현 <silkcoop@gmail.com>
2024-09-24 23:29:49 +0900

    Initial commit

---
+++ lambda_function.py
@@ -0,0 +1,18 @@
+import json
+
+def lambda_handler(event, context):
+    left = event["left"]
+    right = event["right"]
+    result = left ** right
+    
+    print(f"left: {left}")
+    print(f"right: {right}")
+    print(f"result: {result}")
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': 'Hello from Lambda!',
+            'result': result
+        })
+    }
