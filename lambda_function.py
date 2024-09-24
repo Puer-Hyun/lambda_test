b2e8e0bc80195e1837cc429c6a74338d0f23e535
박성현 <silkcoop@gmail.com>
2024-09-25 01:53:07 +0900

    Fix: lambda_function for api gateway

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     body = json.loads(event["body"])
     left = body["left"]
     right = body["right"]
-    result = left ** right
+    result = left ** right + 100
     
     print(f"left: {left}")
     print(f"right: {right}")
